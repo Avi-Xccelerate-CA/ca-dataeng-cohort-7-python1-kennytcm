a82013ea815740ef656ec81ec4f9972707425622
kennytcm <kennytcmbackup@gmail.com>
2024-07-11 20:09:37 +0800

    First

--- week0.py
+++ week0.py
@@ -13,6 +13,26 @@
 # HINT: using % operator to find remainder may be helpful
 def dose(needs):
     #YOUR SOLUTION STARTS HERE
+    pro =['HP','Attack','Defense','Special Attack','special Defense','Speed']
+    b = []
+    sum = 0
+    max = 0
+    for i in range(len(pro)):
+        sum += needs[i]
+        max = needs[i]
+        if sum >= 500:
+            return('No medicine given')
+        if max >= 250:
+            return('No medicine given')
+        if int(needs[i]) == 0:
+            c = 0,0
+            b.append(c)
+        elif int(needs[i])%10 == 0:
+            c = int(needs[i]/10),0
+            b.append(c)
+        else:
+            c = int(needs[i]/10)+1,10-int(needs[i])%10
+            b.append(c)
+    return(b)
 
     #YOUR SOLUTION ENDS HERE
-
